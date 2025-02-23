fb5d5395f911f9676321ddc98ad15eca295cb2ed
Darrel Toledo <dt23aaq@herts.ac.uk>
2025-02-23 21:56:58 +0000

    edges variables

--- workSheet4.py
+++ workSheet4.py
@@ -17,3 +17,11 @@ v3 = Node('v3')
 v4 = Node('v4')
 v5 = Node('v5')
 v6 = Node('v6')
+
+# Add edges with weights
+v1.adjacent = {v2: 1}
+v2.adjacent = {v1: 1, v3: 2, v4: 3, v6: 6}
+v3.adjacent = {v2: 2, v4: 4}
+v4.adjacent = {v2: 3, v3: 4, v5: 5, v6: 7}
+v5.adjacent = {v4: 5}
+v6.adjacent = {v2: 6, v4: 7}
